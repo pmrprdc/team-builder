4eac3a8cedc67909d7d57215f60f3dbfb0c35ed1
Pamir Perdeci <pamirperdeci@gmail.com>
2023-02-05 03:24:46 -0500

    mvp achieved

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import logo from './logo.svg';
 import './App.css';
-import { Form, Input, DreamDiv } from "./StyledComponentsForApp"
+import { Form, Input, DreamDiv, Member } from "./StyledComponentsForApp"
 import React, { useState } from 'react';
+import { isCompositeComponent } from 'react-dom/test-utils';
 
 const initialDreamTeam = ["Elon Musk", "Ray Kurzweil", "Post Malone"]
 
@@ -9,18 +10,44 @@ const initialDreamTeam = ["Elon Musk", "Ray Kurzweil", "Post Malone"]
 function App() {
 
 const [dreamTeam, setDreamTeam] = useState(initialDreamTeam)
+const [formValues, setFormValues] = useState({firstName: "", lastName: ""})
 
+const updateForm =(evt)=>{
+  evt.preventDefault()
+  setFormValues(previousValues =>{
+    return {...previousValues, [evt.target.name]:evt.target.value}
+  })
+  
+}
 
+const handleSubmit = (evt) => {
+  evt.preventDefault();
+  const newMember = `${formValues.firstName} ${formValues.lastName}`
+ setDreamTeam(oldDreamTeam =>{
+  return [...oldDreamTeam, newMember]
+ })
+
+ setFormValues({firstName:"", lastName:""})
+
+}
+  
   return (
     <div className="App">
       <header className="App-header">
         <DreamDiv>
             <h1>Dream Team</h1>
         </DreamDiv>
+        {dreamTeam.map((member, idx)=>{
+          return(
+            <Member key={idx}>
+              <h1>{member}</h1>
+            </Member>
+          )
+        })}
         <img src={logo} className="App-logo" alt="logo" />
-      <Form>
-        <Input type="text" placeholder='First Name'/>
-        <Input type="text" placeholder = 'LastName'/>
+      <Form onSubmit={handleSubmit}>
+        <Input autoFocus value ={formValues.firstName}name ="firstName" onChange={updateForm} type="text" placeholder='First Name'/>
+        <Input value={formValues.lastName} name ="lastName" onChange={updateForm}type="text" placeholder = 'LastName'/>
         <Input type= "submit" value="Add new Team Member"/>
       </Form>
        
--- src/StyledComponentsForApp.js
+++ src/StyledComponentsForApp.js
@@ -6,18 +6,37 @@ const Form = styled.form`
 background-color: green;
 width: 50%;
 border-radius: 20px;
+padding: 10px 0px 10px 0px;
+
 `
 const Input = styled.input`
 background-color: pink;
-border-radius: 5px;
+border-radius: 8px;
+color: yellow;
+font-size: 1rem;
+font-weight: 599;
+margin: 5px;
+
 `
 const DreamDiv = styled.div`
 background-color: green;
 border-radius: 20px;
 width: 50%;
+
+`
+
+
+const Member = styled.div`
+
+    background-color: pink;
+    width: 50%;
+    color: yellow;
+    font-size: 0.5rem;
+    border-radius: 20px;
+    margin: 5px;
 `
 
 
 
 
-export { Form, Input, DreamDiv };
+export { Form, Input, DreamDiv, Member};
