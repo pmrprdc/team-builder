a680bc8a4e1a3acc90fb52ca66cb14d8bc28a916
Pamir Perdeci <pamirperdeci@gmail.com>
2023-02-04 23:35:00 -0500

    created a styled form component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import styled from "styled-components"
+
+
+
+const Form = styled.form`
+background-color: green;
+width: 50%;
+border-radius: 20px;
+
+`
 
 function App() {
   return (
     <div className="App">
+      <Form>
+
+        <input type="text" />
+      </Form>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
