a0cbe6344323010837dd579d372741a0a84d6076
Pamir Perdeci <pamirperdeci@gmail.com>
2023-02-05 02:06:26 -0500

    moved styled components intro their own file named StyledComponenetsForapp.js

--- src/App.js
+++ src/App.js
@@ -1,38 +1,29 @@
 import logo from './logo.svg';
 import './App.css';
 import styled from "styled-components"
+import { Form, Input, DreamDiv } from "./StyledComponentsForApp"
 
 
 
-const Form = styled.form`
-background-color: green;
-width: 50%;
-border-radius: 20px;
-`
-const Input = styled.input`
-background-color: pink;
-border-radius: 5px;`
+
+
+
 
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <DreamDiv>
+            <h1>Test</h1>
+        </DreamDiv>
         <img src={logo} className="App-logo" alt="logo" />
       <Form>
-        <Input type="text" />
+        <Input type="text" placeholder='First Name'/>
+        <Input type="text" placeholder = 'LastName'/>
+        <Input type= "submit" value="Add new Team Member"/>
       </Form>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+       
       </header>
     </div>
   );
---
+++ src/StyledComponentsForApp.js
@@ -0,0 +1,23 @@
+import styled from "styled-components"
+
+
+
+const Form = styled.form`
+background-color: green;
+width: 50%;
+border-radius: 20px;
+`
+const Input = styled.input`
+background-color: pink;
+border-radius: 5px;
+`
+const DreamDiv = styled.div`
+background-color: green;
+border-radius: 5px;
+width: 90%;
+`
+
+
+
+
+export { Form, Input, DreamDiv };
